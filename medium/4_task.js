7958bd609209bca4af5234b38b9a0837746a6899
HovHrach <hovhrachya93@gmail.com>
2019-10-05 15:34:12 +0400

    pushing complate tasks

--- medium/4_task.js
+++ medium/4_task.js
@@ -5,7 +5,36 @@
 
 {
     //// 
-
+    const async1 = (cb) => {
+        setTimeout(() => {
+            cb(1);
+        }, 4000);
+    };
+    const async2 = (cb) => {
+        setTimeout(() => {
+            cb(13);
+        }, 6000);
+    };
+    const async3 = (cb) => {
+        setTimeout(() => {
+            cb(5);
+        }, 3000);
+    };
+    
+    const callBack = (val) => console.log(val);
+    
+    function sendReturningData() {
+        let count = 0;
+        let i = 0;
+        const args = arguments;
+        const cb = res => (++count) === 3 ? args[args.length - 1](res) : 1
+        while (i < args.length - 1) {
+            args[i](cb)
+            i++;
+        }
+    }
+    
+    sendReturningData(async1, async2, async3, callBack);
     ////
 }
 
@@ -17,28 +46,22 @@
 
 {
     //// 
-    const promise1 = new Promise(resolve => {
-        setTimeout(() => {
-            resolve('promise1');
-        }, 2000);
-    });
-    const promise2 = new Promise(resolve => {
-        setTimeout(() => {
-            resolve('promise2');
-        }, 4000);
-    });
-    const promise3 = new Promise(resolve => {
-        setTimeout(() => {
-            resolve('promise3');
-        }, 3000);
-    });
-
-
-    const returnLastPromise = new Promise(() => {
-        Promise.all([promise1, promise2, promise3])
-            .then(result => console.log(result[result.length - 1]))
-            .catch(err => console.log('Catch', err));
-    });
+    const promise1 = new Promise((resolve, reject) => setTimeout(() => resolve('promise1'), 2000));
+    const promise2 = new Promise((resolve, reject) => setTimeout(() => resolve('promise2'), 4000));
+    const promise3 = new Promise((resolve, reject) => setTimeout(() => resolve('promise3'), 3000));
+    
+    const returnLastPromise = args => new Promise((resolve, reject) => {
+        let count = 0;
+        let i = 0;
+        const res = (res) => (++count) === 3 ? resolve(res) : 1
+        const rej = err => err ? reject(err) : 1 
+        while(i < args.length){
+            args[i].then(res, rej)
+            i++
+        }
+    })
+    
+    returnLastPromise([promise1, promise2, promise3]).then((res) => console.log(res), (err) => console.log(err));
     ////
 }
 
@@ -49,96 +72,24 @@
 
 {
     //// 
- ///callBack
-    const firstCallBack = callBack => {
-        console.log("First callback loading");
-        setTimeout(() => {
-            callBack(true);
-        }, 2000);
-    };
+    const observer1 = Rx.Observable(observer => setTimeout(() => observer.next('observer1'), 2000));
+    const observer2 = Rx.Observable(observer => setTimeout(() => observer.next('observer2'), 4000));
+    const observer3 = Rx.Observable(observer => setTimeout(() => observer.next('observer3'), 3000));
 
-    const secondCallBack = callBack => {
-        console.log("Second callback is loading");
-        setTimeout(() => {
-            callBack(true);
-        }, 2000);
-    };
-
-
-    const lastCallBack = () => {
-        firstCallBack((callBack) => {
-            if (callBack) {
-                secondCallBack(cb => {
-                    if (cb) {
-                        result.textContent = "Last callback is loading";
-                    }
-                })
-            }
-        })
-    }
-
-///promise
-    const firstPromise = () => {
-        console.log("First promise loading");
-        return new Promise(resolve => {
-            setTimeout(() => {
-                resolve(true);
-            }, 2000);
-        });
-    };
-
-    const secondPromise = () => {
-        console.log("Second promise loading");
-        return new Promise(resolve => {
-            setTimeout(() => {
-                resolve(true);
-            }, 2000);
-        });
-    };
-
-    const lastPromise = () => {
-        firstPromise().
-            then(isTrue => {
-                if (isTrue) {
-                    return secondPromise().
-                        then(() => console.log("Last promise is loading"))
-                }
-            })
-    }
-
-///observable
-    const firstObserve = () => {
-        console.log("First Observable is loading");
-        return Rx.Observable.create(observer => {
-            setTimeout(() => {
-                observer.next(true);
-            }, 2000);
-        })
-    };
-
-    const secondObserve = () => {
-        return Rx.Observable.create(observer => {
-            console.log("Second Observable is loading");
-            setTimeout(() => {
-                observer.next(true);
-            }, 2000);
-        })
-    };
-
-    const lastObserve = () => {
-        Rx.Observable
-            .switchMap(() => firstObserve())
-            .switchMap(isTrue => {
-                if (isTrue) {
-                    return secondObserve()
-                }
-            })
-            .subscribe(() => {
-                console.log("Last Observable is loading");
-            })
-    }
-
-    ////
+    const returnLastObservable = args => Rx.Observable(observer => {
+        let count = 0;
+        let i = 0;
+        const obs = obs => (++count) === 3 ? observer(obs) : 1
+        while (i < args.length) {
+            args[i].subscribe.next(obs)
+            i++
+        }
+    })
+    returnLastObservable().subscribe({
+        next: [observer1, observer2, observer3],
+        complete: obs => console.log(obs)
+    });
+   ////
 }
 
 
